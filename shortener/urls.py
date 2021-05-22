5b51ab244d1a058694f90ddf9eb6f02a16cb5adb
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-05-22 23:15:34 +0100

    Chore: Reworked path for API

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 app_name = 'shortener'
 
 urlpatterns = [
-    path('create/', views.LinkCreateAPIView.as_view(), name='create-link'),
+    path('api/', include([
+        path('create/', views.LinkCreateAPIView.as_view(), name='create-link'),
+    ])),
     path('<slug:path>/', views.LinkRedirectView.as_view(), name='redirect-link'),
 ]
