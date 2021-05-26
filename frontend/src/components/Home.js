889fa44a01fdf1c4cc0f95556a9b1c5b05474bdd
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-05-26 17:48:48 +0100

    Chore: Reduced font size

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -13,7 +13,7 @@ const MainImage = () => {
 }
 const MainTitle = ({ text }) => {
     return (
-        <h1 className="font-sans text-3xl my-3 text-center font-bold">
+        <h1 className="font-sans text-5xl my-3 text-center font-bold">
             {text}
         </h1>
     )
@@ -21,7 +21,7 @@ const MainTitle = ({ text }) => {
 
 const MainDescription = ({ text, blueText }) => {
     return (
-        <p className="p-1 text-center text-gray-500 w-4/5 mt-2 mb-4 text-3xl">
+        <p className="p-1 text-center text-gray-500 w-4/5 mt-2 mb-4 text-2xl">
             {text} <span className="text-blue-500">{blueText}</span>
         </p>
     )
@@ -32,14 +32,14 @@ const LinkForm = ({ placeHolder }) => {
         <input 
          type="url"
          placeholder={placeHolder}
-         className="border-0 w-full md:w-3/5 px-8 py-4 text-gray-400 bg-gray-100">
+         className="border-0 w-full md:w-3/5 px-8 py-3 text-gray-400 bg-gray-100">
         </input>
     )
 }
 
 const ShortenButton = ({ text }) => {
     return (
-        <button className="border-0 mt-4 md:my-auto md:mx-2 w-full md:w-auto bg-none bg-blue-400 px-8 py-4 rounded-none text-white hover:bg-blue-500">
+        <button className="border-0 text-lg mt-4 md:my-auto md:mx-2 w-full md:w-auto bg-none bg-blue-400 px-8 py-3.5 rounded-none text-white hover:bg-blue-500">
             {text}
         </button>
     )
